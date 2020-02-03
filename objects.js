f952ec4da50d02c1f7d475cb1f52db095eb66e60
Kira <kiramurphy13@yahoo.com>
2020-02-03 13:07:40 -0500

    more js objects

--- objects.js
+++ objects.js
@@ -2,4 +2,16 @@
 
 // Object literal
 const point = {x: 1, y:2};
-console.log(point)
+console.log(point);
+
+console.log(point.x);
+
+point.x = 3;
+
+const circle = {
+  center: point;
+  radius: 4
+};
+
+console.log(circle.center)
+console.log(circle.radius)
