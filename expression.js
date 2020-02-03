ef82c716adc646db076bc5c9339d21756bf70a25
Kira <kiramurphy13@yahoo.com>
2020-02-03 11:01:35 -0500

    js expressions

---
+++ expression.js
@@ -0,0 +1,55 @@
+// Expression syntax in JavaScript
+
+// Booleans
+true
+false
+
+// Logic
+!false
+true & false
+true or false
+
+// Numbers(all one type)
+3.14
+
+// Equality
+3 === 3.0
+3!== 3.14
+
+// Ordering
+3 > 2
+3 >= 3
+2 < 3
+2 <= 3
+
+// Stirngs
+'hello'
+
+// Concatenate
+ 'a' + 'b' === 'ab'
+
+// Arithmetic
+3 + 2 === 5
+3 - 1 === 1
+3 * 2 === 6
+3 / 2 === 1.5
+3 % 2 === 1
+3 ** 2 === 9
+
+// Types
+
+'3' + 2 === '32'
+'3' + true ==='3true'
+'3' - 2 === 1
+3 + true === 4
+
+// Weaker equality
+'3' == 3
+true == 1
+false == 0
+false == ''
+'' == 0
+
+// Template strings
+'3 + 2 is ' + (3 + 2)
+`3 + 2 is ${3 + 2}`
