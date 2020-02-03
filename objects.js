9f51491ca1cc226e94c373c572eb92377d6e313f
Kira <kiramurphy13@yahoo.com>
2020-02-03 13:23:54 -0500

    functions practice

--- objects.js
+++ objects.js
@@ -19,6 +19,31 @@ console.log(circle.radius)
 // Array
 const languages = ['HTML', 'CSS', 'JS']
 console.log(languages)
+
+// Accessing length and elements
 for(let i=0; i < languages.length; i++){
   console.log(languages[i]);
 }
+
+// Changing elements
+languages[2] = 'JavaScript'
+
+languages.push('MongoDB')
+
+for (const lang of languages){
+  console.log(lang)
+}
+
+const avg = function(a,b) {
+  return (a + b) / 2;
+};
+
+const print = function(a,b) {
+  console.log(`a=${a}, b=${b}`);
+};
+
+// Undefined values
+const result = print(7,8);
+console.log(result); // Logs undefined
+print(9,10); // 9, undefined
+print() // undefined, undefined
