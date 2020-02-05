a25c64bbc94446eaa45eb41bc9406a4c618c50d2
Kira <kiramurphy13@yahoo.com>
2020-02-05 13:27:30 -0500

    added console log

---
+++ callbacks.js
@@ -0,0 +1,20 @@
+// Syntax for callbacks
+
+// This function uses another function to trasnform an array
+const map = function(array, transform){
+  const copy = []
+  for(const element of array){
+    copy.push(transform(element))
+  }
+  return copy;
+}
+
+const square = function(x){
+  return x**2;
+}
+
+const numbers = [1,2,3,4,5];
+
+
+console.log(map(numbers, square));
+console.log(numbers);
