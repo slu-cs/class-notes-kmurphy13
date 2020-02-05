0c78e772ed3eeedafa183c7d87ac9ab9772f2b2d
Kira <kiramurphy13@yahoo.com>
2020-02-05 14:00:11 -0500

    read file

--- javascript/input.js
+++ javascript/input.js
@@ -1,5 +1,6 @@
 // Syntax for console and file ouput
 const readline = require('readline');
+const fs = require('fs');
 
 // Console configuration
 const user = readline.createInterface({
@@ -9,5 +10,18 @@ const user = readline.createInterface({
 
 // Console input
 user.question('Filename: ', function(filename) {
-  console.log(filename);
-})
+
+  // File configuration
+  const file = readline.createInterface({
+    input: fs.createReadStream(filename)
+  });
+
+  // Asynchronous line-by-line input
+  file.on('line', function(line)){
+    console.log(line);
+  });
+
+  file.on('close', function(){
+    process.exit(0);
+  });
+});
