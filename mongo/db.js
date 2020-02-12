cb0a1ccccaf86c903d378245029b3089d162b429
Kira <kiramurphy13@yahoo.com>
2020-02-12 13:08:57 -0500

    mongoose

--- mongo/db.js
+++ mongo/db.js
@@ -3,21 +3,19 @@
 const mongoose = require('mongoose');
 
 // Export this function
-module.exports = function () {
+module.exports = function() {
 
-    // Avoid warnings
-    mongoose.set('useNewUrlParser', true);
-    mongoose.set('useUnifiedTopology', true);
-    mongoose.set('useCreateIndex', true);
-    mongoose.set('useFindAndModify', false);
+  // Avoid warnings
+  mongoose.set('useNewUrlParser', true);
+  mongoose.set('useUnifiedTopology', true);
+  mongoose.set('useCreateIndex', true);
+  mongoose.set('useFindAndModify', false);
 
-    // Start connecting
-    mongoose.connect('mongodb://localhost/faculty');
+  // Start connecting
+  mongoose.connect('mongodb://localhost/faculty');
 
-    // Make sure we see any errors
-    mongoose.connection.on('error', function (error) {
-        console.error(error.stack);
-    });
-
-}
-
+  // Make sure we see any errors
+  mongoose.connection.on('error', function(error) {
+    console.error(error.stack);
+  });
+};
