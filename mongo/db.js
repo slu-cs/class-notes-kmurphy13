f5f0cc600699ba924f7abbcd5e952caf04a49604
Kira <kiramurphy13@yahoo.com>
2020-02-12 13:23:46 -0500

    typical mongodb queries

--- mongo/db.js
+++ mongo/db.js
@@ -19,3 +19,4 @@ module.exports = function() {
     console.error(error.stack);
   });
 };
+
--- mongo/query.js
+++ mongo/query.js
@@ -14,8 +14,58 @@ query.exec(function(error, professors) {
   console.log(professors);
 });
 
+const queries = [
 
-const queries ={
+    // Sort professors by name
     Professor.find().sort('name'),
-    Professor.find().sort('-started')
-}
+
+    // Reverse sort professors on date started 
+    Professor.find().sort('-started'),
+
+    // Find Professors that started in 2003
+    Professor.find().where('started').equals(2003),
+
+    // Professors who have taught 362
+    Professor.find().where('courses').in(362),
+
+    // What are all the ranks
+    Professor.distinct('rank')
+]
+
+queries[0].exec(function(error, professors) {
+    if(error) console.error(error.stack);
+
+    const names = professors.map(p => p.name);
+    console.log('Names in order: ', names);
+});
+
+queries[1].exec(function(error, professors) {
+    if(error) console.error(error.stack);
+    
+    const names = professors.map(p => p.name);
+    console.log('Started most recently: ', names);
+});
+
+queries[2].exec(function(error, professors) {
+    if(error) console.error(error.stack);
+    
+    const names = professors.map(p => p.name);
+    console.log('Started in 2003', names);
+});
+
+queries[3].exec(function(error, professors) {
+    if(error) console.error(error.stack);
+    
+    const names = professors.map(p => p.name);
+    console.log('Professors who have taught CS362: ', names);
+});
+
+queries[4].exec(function(error, professors) {
+    if(error) console.error(error.stack);
+    
+    console.log('Possible ranks: ', names);
+});
+
+
+
+
